5b35e4f7878fbf164125a0dd3a50ec0b4619b792
tejeswar-sai <tejeswarsai.v@gmail.com>
2024-12-22 18:41:47 +0530

    updated cors hosts in settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fo65!zizafd2!r%l45bp8147-((j$zt0k1gd0sl9*=265_@or+
 DEBUG = False
 
 ALLOWED_HOSTS = ['44.211.216.122','192.168.1.13','127.0.0.1']
-CORS_ALLOWED_ORIGINS = ["http://192.168.1.13:3000",]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000",]
 
 
 # Application definition
